3278578a5f2596fcff5983df25be9c7b136b6507
sergio <sergio.martinez@studio73.es>
2025-01-24 10:39:16 +0100

    [MIG] mail_layout_force: Migration to 17.0

--- mail_layout_force/models/mail_template.py
+++ mail_layout_force/models/mail_template.py
@@ -2,7 +2,7 @@
 # @author Iván Todorovich <ivan.todorovich@camptocamp.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MailTemplate(models.Model):
@@ -16,29 +16,8 @@ class MailTemplate(models.Model):
         help="Force a mail layout for this template.",
     )
 
-    def _ensure_force_email_layout_xml_id(self):
-        missing = self.force_email_layout_id.filtered(lambda rec: not rec.xml_id)
-        if missing:
-            vals = [
-                {
-                    "module": "__export__",
-                    "name": "force_email_layout_%s" % rec.id,
-                    "model": rec._name,
-                    "res_id": rec.id,
-                }
-                for rec in missing
-            ]
-            self.env["ir.model.data"].sudo().create(vals)
-            self.force_email_layout_id.invalidate_recordset(["xml_id"])
+    email_layout_xmlid = fields.Char(compute="_compute_email_layout_id")
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        records = super().create(vals_list)
-        records._ensure_force_email_layout_xml_id()
-        return records
-
-    def write(self, vals):
-        res = super().write(vals)
-        if "force_email_layout_id" in vals:
-            self._ensure_force_email_layout_xml_id()
-        return res
+    def _compute_email_layout_id(self):
+        for template in self:
+            template.email_layout_xmlid = template.force_email_layout_id.xml_id
